c8eb001958fb5a058d50ada5b7b95fadba9ef3b4
vladtopolev <vladimir.topolev@numatic.co>
2025-07-29 16:37:31 +0200

    add google analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -4,16 +4,16 @@ import { cn } from '@/_frontend/shared/lib/cn'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
 import React from 'react'
+import Script from 'next/script'
 
-import { Footer } from '@/Footer/Component'
 import { Providers } from '@/_frontend/app/providers'
 import { InitTheme } from '@/_frontend/shared/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import { draftMode } from 'next/headers'
 
 import '@/_frontend/app/styles/globals.css'
-import { getServerSideURL } from '@/utilities/getURL'
 import { Header } from '@/_frontend/shared/ui/header'
+import { getServerSideURL } from '@/utilities/getURL'
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { isEnabled } = await draftMode()
@@ -23,6 +23,28 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <head>
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        {process.env.GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+            />
+            <Script
+              id="ga-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+              }}
+            />
+          </>
+        )}
       </head>
       <body>
         <Providers>
