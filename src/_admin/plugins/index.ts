e29c710d0cb0fc17107af2e385a9b6ce75045568
vladtopolev <vladimir.topolev@numatic.co>
2025-08-04 10:13:42 +0200

    fix title seo

--- src/_admin/plugins/index.ts
+++ src/_admin/plugins/index.ts
@@ -16,7 +16,7 @@ import { Page, Post } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
 
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
-  return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+  return doc?.title ? `${doc.title} | TypeScript Challenges` : 'TypeScript Challenges'
 }
 
 const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
--- src/endpoints/seed/home.ts
+++ src/endpoints/seed/home.ts
@@ -47,7 +47,7 @@ export const home: (args: HomeArgs) => RequiredDataFromCollectionSlug<'pages'> =
                   format: 0,
                   mode: 'normal',
                   style: '',
-                  text: 'Payload Website Template',
+                  text: 'TypeScript Challenges',
                   version: 1,
                 },
               ],
@@ -668,7 +668,7 @@ export const home: (args: HomeArgs) => RequiredDataFromCollectionSlug<'pages'> =
     meta: {
       description: 'An open-source website built with Payload and Next.js.',
       image: heroImage.id,
-      title: 'Payload Website Template',
+      title: 'TypeScript Challenges',
     },
     title: 'Home',
   }
